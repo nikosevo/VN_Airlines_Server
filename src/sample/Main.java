0f636202a2cf2bb46a90af7539085ec5ab71b435
balantis <balantispat@gmail.com>
2021-04-23 20:24:54 +0300

    change 1

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -1,8 +1,11 @@
 package sample;
 
-public class Main {
+public class Main
+{
 
-    public static void main(String[] args) {
-        System.out.println("hi valnik");
+    public static void main(String[] args)
+    {
+        Person per = new Person("val","@test","12","chios","152","12a"); //test subject
+        System.out.println(per.toString());
     }
 }
