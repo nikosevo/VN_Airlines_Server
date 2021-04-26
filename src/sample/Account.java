0512f40331ce55170f47cc43fe3aab8e0cca6df7
balantis <balantispat@gmail.com>
2021-04-26 22:00:26 +0300

    checkAvailanility

---
+++ src/sample/Account.java
@@ -0,0 +1,22 @@
+package sample;
+
+import java.util.ArrayList;
+
+public class Account
+{
+    private String password;              //this is going to be hashed ;)
+    private String username;
+    private String userId;
+    private String age;
+    private String lastdest;  //user last used/visited destination
+    private String email;
+    private ArrayList<String[]> reservations ;
+
+    public Account(String password,String username,String userId,String age,String lastdest,String email)
+    {
+        this.age=age;
+        this.email=email;
+
+    }
+
+}
